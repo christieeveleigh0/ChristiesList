4489d0b94abb18570404c65affaa31a18077c4e0
christieeveleigh0 <39589003+christieeveleigh0@users.noreply.github.com>
2022-03-22 08:48:05 +0200

    Completing Registration functionality

--- src/core/queries.php
+++ src/core/queries.php
@@ -1,15 +1,5 @@
 <?php
 
-// $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
-// $stmt->bind_param("sss", $firstname, $lastname, $email);
-// $firstname = "John";
-// $lastname = "Doe";
-// $email = "john@example.com";
-// $stmt->execute();
-// $stmt->close();
-// $conn->close();
-
-
 /* Select all listings from database and return them to funciton call.
    There is no need to make this a prepared statement because nothing is being inserted. */
 function getAllListings() {
@@ -35,7 +25,8 @@ function getListingsByCategory($category) {
     return $conn->query("SELECT * FROM listing WHERE category='" . $category . "'");
 }
 
-/* Select the last 8 results to display in the Hot section on the Home page. The first 4 listings are dummies and will always be there. */
+/* Select the last 8 results to display in the Hot section on the Home page. The first 4 listings are 
+   dummies and will always be there. */
 function getHotListings() {
     require 'conn.php';
     return $conn->query("SELECT * FROM listing WHERE id > 5");
@@ -47,3 +38,29 @@ function getHash($email) {
     return $conn->query("SELECT id, password FROM users WHERE email='" . $email . "'");
 }
 
+/* Select the corresponding email address to check if the user has an account or not */
+function checkIfRegistered($email) {
+    require 'conn.php';
+    $result = $conn->query("SELECT * FROM users WHERE email='$email'");
+    if (mysqli_num_rows($result) != 0) { return true; }
+    return false;
+}
+
+/* Perform a prepared statement for security reasons. Prepare and bind variables.
+   On successful insertion, return true */
+function registerUser($name, $number, $email, $password) {
+    require 'conn.php';
+    $query = "INSERT INTO users (name, number, email, password) VALUES (?, ?, ?, ?)";
+
+    if($stmt = mysqli_prepare($conn, $query)) {
+        mysqli_stmt_bind_param($stmt, "ssss", $name, $number, $email, $password);
+        mysqli_stmt_execute($stmt);
+        return true;
+    } else {
+        echo mysqli_error($link);
+        return false;
+    }
+
+    mysqli_stmt_close($stmt); // Close connections
+    mysqli_close($conn);
+}
